5ae077656e70c4fe8059eba6920ba68f812eb7fc
Islam Zaky <islamzaky33@gmail.com>
2025-12-07 02:48:01 +0200

    Initial commit

---
+++ src/_1_Variables.java
@@ -0,0 +1,9 @@
+public class _2_Variables {
+    public static void main(String[] args) {
+        int x;  //variable declaration(store 4 Bytes memory to int)
+        x=10;   //variable initialization
+
+        x=7;   //assignment operator after initialization
+        System.out.println(x);
+    }
+}
---
+++ src/_2_DataTypes.java
@@ -0,0 +1,27 @@
+public class _1_DataTypes {
+    public static void main(String[] args) {
+        //Whole Numbers
+        byte by=127;
+        System.out.println("the value of byte variable is " +by);
+        short sh = 32666;
+        System.out.println("Short variable is: "+sh);
+        int i = 222;
+        System.out.println("value of int variable is:"+i);
+        long l = 555555L;
+        System.out.println("value of long variable is:"+l);
+
+        //Decimal Numbers
+        float fl = 7.5f;
+        System.out.println("value of float variable is: "+fl);
+
+        double d = 3.5;
+        System.out.println("value of double variable: "+d);
+
+        boolean bl = true;
+        System.out.println("value of bool variable: "+bl);
+
+        char ch = 'E';
+        System.out.println("value of char variable is: "+ch);
+
+    }
+}
